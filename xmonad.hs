c4fa112637581cf68c4690ea4417d4ee242b6f94
Hizbullah Abdul Aziz <hizbullah.abdul@gmail.com>
2016-10-17 16:40:21 +0200

    Revert to using greedyView

--- xmonad.hs
+++ xmonad.hs
@@ -8,30 +8,29 @@ import qualified XMonad.StackSet as W
 
 main :: IO ()
 main = xmonad def
-  { terminal    = "st"
+  { terminal    = "termite -e fish"
   , modMask     = mod4Mask
-  , borderWidth = 0
+  , borderWidth = 2
   , keys        = \c -> keys' c `M.union` keys def c
   , manageHook  = manageHook' <+> manageHook def
-  , workspaces  = withScreens 2 $ map show [1..9]
   }
     where
       keys' conf = let modm = modMask conf in M.fromList $
         [ ((modm, xK_p),                  spawn "dmenu_run -fn 'Inconsolata-12'")
         , ((0, xF86XK_MonBrightnessUp),   spawn "light -A 5")
         , ((0, xF86XK_MonBrightnessDown), spawn "light -U 5")
-        , ((0, xF86XK_AudioMute),         spawn "pamixer -t")
-        , ((0, xF86XK_AudioRaiseVolume),  spawn "pamixer -i 5")
-        , ((0, xF86XK_AudioLowerVolume),  spawn "pamixer -d 5")
+        , ((0, xF86XK_AudioMute),         spawn "ponymix toggle")
+        , ((0, xF86XK_AudioRaiseVolume),  spawn "ponymix increase")
+        , ((0, xF86XK_AudioLowerVolume),  spawn "ponymix decrease")
         ]
         ++
-        [ ((m .|. modm, k), windows $ onCurrentScreen f i)
-          | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
-          , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+        [ ((m .|. modm, k), windows $ f i)
+          | (i, k) <- zip (workspaces conf) [xK_1 .. xK_9]
+          , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
         ]
 
 
       manageHook' = composeAll
         [ className =? "chromium" --> doShift "2"
-        , className =? "Atom"     --> doShift "3"
+        , className =? "Emacs"    --> doShift "3"
         ]
