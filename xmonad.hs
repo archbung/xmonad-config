1c8f9f3aec75ce3536b6f063624f606d2b6d71c9
Hizbullah Abdul Aziz <hizbullah.abdul@gmail.com>
2017-02-12 11:43:21 +0100

    Add minor fixes

--- xmonad.hs
+++ xmonad.hs
@@ -9,17 +9,17 @@ import qualified XMonad.StackSet as W
 
 main :: IO ()
 main = xmonad def
-  { terminal    = "alacritty"
+  { terminal    = "termite -e tmux"
   , modMask     = mod4Mask
-  , borderWidth = 2
+  , borderWidth = 0
   , keys        = \c -> keys' c `M.union` keys def c
   , manageHook  = manageHook' <+> manageHook def
   }
     where
       keys' conf = let modm = modMask conf in M.fromList $
         [ ((modm, xK_p),                  spawn "dmenu_run -fn 'Inconsolata-12'")
-        , ((0, xF86XK_MonBrightnessUp),   spawn "light -A 5")
-        , ((0, xF86XK_MonBrightnessDown), spawn "light -U 5")
+        , ((0, xF86XK_MonBrightnessUp),   spawn "xbacklight +5")
+        , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -5")
         , ((0, xF86XK_AudioMute),         spawn "ponymix toggle")
         , ((0, xF86XK_AudioRaiseVolume),  spawn "ponymix increase 5")
         , ((0, xF86XK_AudioLowerVolume),  spawn "ponymix decrease 5")
