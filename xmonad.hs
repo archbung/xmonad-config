cece8174dbc93da990664cde764386afab36a395
Hizbullah Abdul Aziz <hizbullah.abdul@protonmail.com>
2018-05-10 11:07:54 +0200

    Add a minor cosmetic change

--- xmonad.hs
+++ xmonad.hs
@@ -12,14 +12,14 @@ import qualified XMonad.StackSet as W
 main :: IO ()
 main = xmonad =<< statusBar myBar myPP toggleStrutKey myConfig
 
-myBar = "xmobar"   
+myBar = "xmobar"
 
 myPP  = xmobarPP { ppCurrent = xmobarColor "#d79921" "" . wrap "[" "]" }
 
 toggleStrutKey XConfig { XMonad.modMask = modMask } = (modMask, xK_b)
 
 myConfig = def
-  { terminal    = "alacritty"
+  { terminal    = "termite"
   , modMask     = mod4Mask
   , borderWidth = 2
   , focusFollowsMouse = False
@@ -28,7 +28,7 @@ myConfig = def
   }
     where
       keys' conf = let modm = modMask conf in M.fromList $
-        [ ((modm, xK_p),                  spawn "dmenu_run -fn 'InputMonoCondensed-10' -b")
+        [ ((modm, xK_p),                  spawn "dmenu_run -fn 'Inconsolata-12' -b")
         , ((modm .|. shiftMask, xK_l),    spawn "i3lock -c 000000")
         , ((0, xF86XK_MonBrightnessUp),   spawn "xbacklight +5")
         , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -5")
@@ -39,6 +39,7 @@ myConfig = def
         ]
 
       manageHook' = composeAll
-        [ className =? "Chromium" --> doShift "2"
+        [ className =? "Chromium" --> doShift "3"
         , className =? "Firefox Developer Edition" --> doShift "2"
+        , className =? "Emacs" --> doShift "1"
         ]
