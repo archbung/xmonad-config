36fd60e1f89a8377887b9868c50b7af99ee8ce61
Hizbullah Abdul Aziz <archbung@gmail.com>
2019-12-25 20:13:39 +0100

    Set workspace 9 layout to only full

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import           XMonad.Hooks.DynamicLog
 import           XMonad.Layout.NoBorders
 import           XMonad.Layout.Fullscreen
 import           XMonad.Layout.Spiral
+import           XMonad.Layout.PerWorkspace
 import           XMonad.Util.EZConfig
 import           XMonad.Actions.UpdatePointer
 
@@ -24,10 +25,9 @@ main  = xmonad =<< xmobar (Ewmh.ewmh def
         , appName =? "zenity"   --> doFloat
         , fullscreenManageHook
         ]
-  , layoutHook  = smartBorders $ avoidStruts $
-        (Tall 1 (3/100) (1/2)) |||
-        (spiral (6/7)) |||
-        (noBorders (fullscreenFull Full))
+  , layoutHook  = let full = noBorders (fullscreenFull Full)
+                   in onWorkspace "9" full $ smartBorders $ avoidStruts $
+                        (Tall 1 (3/100) (1/2)) ||| (spiral (6/7)) ||| full
   , logHook     = dynamicLogWithPP def >> updatePointer (0.75, 0.75) (0.75, 0.75)
   , handleEventHook = mconcat
         [ handleEventHook def
@@ -45,4 +45,4 @@ main  = xmonad =<< xmobar (Ewmh.ewmh def
 
     where
         role = stringProperty "WM_WINDOW_ROLE"
-        menu = "rofi -combi-modi window,drun -show combi -modi combi -hide-scrollbar"
+        menu = "rofi -combi-modi window,run -show combi -modi combi -hide-scrollbar"
